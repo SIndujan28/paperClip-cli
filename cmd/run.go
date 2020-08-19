748d40e57da0f667abc961ecd21d0fef045def0f
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-08-20 00:12:50 +0530

    add fn to invoke mutool

--- cmd/run.go
+++ cmd/run.go
@@ -1,21 +1,34 @@
-package main
+package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"os/exec"
 )
 
-func main() {
+//Run fn executes mutool cmd
+func Run(path string, f string) {
 	app := "mutool"
-
-	arg0 := "info"
-
-	cmd := exec.Command(app, arg0, "../toc.txt")
-
-	output, err := cmd.CombinedOutput()
+	arg0 := "show"
+	arg1 := path
+	arg2 := "outline"
+	// Open the output file
+	outfile, err := os.Create(fmt.Sprintf("%s.txt", f))
 	if err != nil {
-		fmt.Println(fmt.Sprint(err) + ": " + string(output))
-		return
+		log.Fatal(err)
 	}
-	fmt.Println(string(output))
+	defer outfile.Close()
+	cmd := exec.Command(app, arg0, arg1, arg2)
+	cmd.Stdout = outfile
+	if err = cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print("Waiting for command to finish...")
+
+	// Wait for the command to finish.
+	if err = cmd.Wait(); err != nil {
+		log.Fatal(err)
+	}
 }
