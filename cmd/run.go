163fa33086b1f6156d7c1300458852115dae7c23
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-08-20 19:46:48 +0530

    integrate parser and producer in cmd/run

--- cmd/run.go
+++ cmd/run.go
@@ -5,16 +5,19 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path"
+
+	"github.com/SIndujan28/paperclip/parser"
 )
 
 //Run fn executes mutool cmd
-func Run(path string, f string) {
+func Run(bpath string) []string {
 	app := "mutool"
 	arg0 := "show"
-	arg1 := path
+	arg1 := bpath
 	arg2 := "outline"
 	// Open the output file
-	outfile, err := os.Create(fmt.Sprintf("%s.txt", f))
+	outfile, err := os.Create(fmt.Sprintf("%s.txt", path.Base(bpath)))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -31,4 +34,6 @@ func Run(path string, f string) {
 	if err = cmd.Wait(); err != nil {
 		log.Fatal(err)
 	}
+	s := parser.Fetch(fmt.Sprintf("%s.txt", f))
+	return s
 }
