c31ae7d03ea47b713075ee6d36e2b073c5999469
SIndujan28 <30890973+SIndujan28@users.noreply.github.com>
2020-08-20 19:46:13 +0530

    make kafka producer returnable fn

--- kafka/kafka-producer.go
+++ kafka/kafka-producer.go
@@ -1,13 +1,33 @@
 package kafka
 
 import (
+	"encoding/json"
 	"fmt"
+	"path"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+type book struct {
+	Name     string   `json:"name"`
+	Path     string   `json:"path"`
+	Version  int      `json:"version"`
+	Keywords []string `json:"keywords"`
+}
+
 //Producer export function
-func Producer(topic string) {
+func Producer(bpath string, keywords []string) {
+
+	result, err := json.Marshal(book{
+		Name:     path.Base(bpath),
+		Path:     bpath,
+		Version:  1,
+		Keywords: keywords,
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost",
 	})
@@ -30,13 +50,12 @@ func Producer(topic string) {
 		}
 	}()
 
+	topic := "topicme"
 	// Produce messages to topic (asynchronously)
-	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(word),
-		}, nil)
-	}
+	p.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          result,
+	}, nil)
 
 	// Wait for message deliveries before shutting down
 	p.Flush(15 * 1000)
